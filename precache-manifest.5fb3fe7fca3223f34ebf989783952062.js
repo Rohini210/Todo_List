81bbfc8b0f1584fc4780115e18a506da101da6dc
Rohini210 <prohini210@gmail.com>
2020-09-07 18:54:50 +0530

    Updates

---
+++ precache-manifest.5fb3fe7fca3223f34ebf989783952062.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb7e8fc042bc985a364b9cad97725e19",
+    "url": "/Todo_List/index.html"
+  },
+  {
+    "revision": "e9fecbd3b1f492f048c9",
+    "url": "/Todo_List/static/css/main.9afe05d4.chunk.css"
+  },
+  {
+    "revision": "a52c874fdf04b3b73bc4",
+    "url": "/Todo_List/static/js/2.e27c2b33.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo_List/static/js/2.e27c2b33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9fecbd3b1f492f048c9",
+    "url": "/Todo_List/static/js/main.404fe896.chunk.js"
+  },
+  {
+    "revision": "2a7ed5b0b481d6b108a2",
+    "url": "/Todo_List/static/js/runtime-main.1d1bc023.js"
+  }
+]);
